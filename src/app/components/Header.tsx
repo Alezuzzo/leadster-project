89d4d256544a2bc3f6d02d551a5049f100945351
Thalles Alexsander <thallesander@hotmail.com>
2023-06-23 21:41:44 -0300

    adicionando e editando header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="headerLandpage">
+      <img src="/img/logo.png" alt="logo Leadster" />
+    </div>
+  );
+}
+
+export default Header;
