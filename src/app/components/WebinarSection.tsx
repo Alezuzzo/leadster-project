b78fb783e9236c12ab45c216f734e6f131ab6f94
Thalles Alexsander <thallesander@hotmail.com>
2023-06-24 21:30:34 -0300

    adicionando e editando seção de webinars

---
+++ src/app/components/WebinarSection.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function WebinarSection() {
+  return (
+    <div className='webinarSection'>
+        <button className='buttonWebinar'>
+            <a href='https://leadster.com.br/?utm_source=branding&utm_medium=cpc&utm_campaign=bofu_pql_CA_search_branding_leadster&utm_term={leadster}&utm_content=CJ_leadster&utm_source=branding&utm_medium=cpc&utm_campaign=Branding_leadster&gad=1&gclid=Cj0KCQjwqNqkBhDlARIsAFaxvwyFW-7HfZDnXND3wWEULLQ28hutU_cDScVVWgbrWOyUUhid65CoQ-oaApiNEALw_wcB'>WEBINARS EXCLUSIVOS</a>
+        </button>
+        <h3>Menos conversinha,</h3>
+        <h1>Mais Conversão</h1>
+        <img src="/img/asset-header.png" alt=""  className='headerLogo'/>
+        <p>Conheça as estratégias que mudaram o jogo e como aplicá-las no seu negócio</p>
+    </div>
+  )
+}
+
+export default WebinarSection
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Header from './components/Header'
+import WebinarSection from './components/WebinarSection'
 
 export default function Home() {
   return (
     <main>
       <div>
         <Header/>
+        <WebinarSection/>
       </div>
     </main>
   )
